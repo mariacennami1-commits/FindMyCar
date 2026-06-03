8fa3c9f29fc14376a2c3184773a01edecd62a0a9
Maria Cennami <maria.cennami1@gmail.com>
2026-06-03 21:06:47 +0200

    Add setup.py.patch for Pillow cross-compilation (disable host lib detection)

--- .p4a/recipes/Pillow/__init__.py
+++ .p4a/recipes/Pillow/__init__.py
@@ -1,3 +1,5 @@
+from os.path import join
+
 from pythonforandroid.recipe import PyProjectRecipe
 
 
@@ -5,9 +7,39 @@ class PillowRecipe(PyProjectRecipe):
     version = '12.2.0'
     url = 'https://github.com/python-pillow/Pillow/archive/{version}.tar.gz'
     site_packages_name = 'PIL'
+    patches = ["setup.py.patch"]
     depends = ['png', 'jpeg', 'freetype']
     hostpython_prerequisites = ["setuptools>=77"]
     opt_depends = ['libwebp']
+
+    def get_recipe_env(self, arch, **kwargs):
+        env = super().get_recipe_env(arch, **kwargs)
+
+        env["LDFLAGS"] = env.get("LDFLAGS", "") + " -lm"
+
+        jpeg = self.get_recipe('jpeg', self.ctx)
+        jpeg_inc_dir = jpeg_lib_dir = jpeg.get_build_dir(arch.arch)
+        env["JPEG_ROOT"] = "{}:{}".format(jpeg_lib_dir, jpeg_inc_dir)
+
+        freetype = self.get_recipe('freetype', self.ctx)
+        free_lib_dir = join(freetype.get_build_dir(arch.arch), 'objs', '.libs')
+        free_inc_dir = join(freetype.get_build_dir(arch.arch), 'include')
+        env["FREETYPE_ROOT"] = "{}:{}".format(free_lib_dir, free_inc_dir)
+
+        harfbuzz = self.get_recipe('harfbuzz', self.ctx)
+        harf_lib_dir = join(harfbuzz.get_build_dir(arch.arch), 'src', '.libs')
+        harf_inc_dir = harfbuzz.get_build_dir(arch.arch)
+        env["HARFBUZZ_ROOT"] = "{}:{}".format(harf_lib_dir, harf_inc_dir)
+
+        env["ZLIB_ROOT"] = f"{arch.ndk_lib_dir_versioned}:{self.ctx.ndk.sysroot_include_dir}"
+
+        if 'libwebp' in self.ctx.recipe_build_order:
+            webp = self.get_recipe('libwebp', self.ctx)
+            webp_install = join(
+                webp.get_build_dir(arch.arch), 'installation'
+            )
+            env["WEBP_ROOT"] = f"{join(webp_install, 'lib')}:{join(webp_install, 'include')}"
+        return env
 
 
 recipe = PillowRecipe()
